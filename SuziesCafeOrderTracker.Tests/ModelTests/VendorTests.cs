aa74de0f8735bafb92c043ae2240bf52127eaa86
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-09-23 19:37:24 -0700

    removed line

--- SuziesCafeOrderTracker.Tests/ModelTests/VendorTests.cs
+++ SuziesCafeOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -4,19 +4,17 @@ using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Tests.ModelTests
 {
-    [TestClass]
-    public class VendorTests
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-        [TestMethod]
-        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-        {
-            // Arrange, Act
-            Vendor newVendor = new Vendor();
+      // Arrange, Act
+      Vendor newVendor = new Vendor();
 
-            // Assert
-            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-        }
-
-        // Add more test methods for Vendor class as needed.
+      // Assert
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+  }
 }
