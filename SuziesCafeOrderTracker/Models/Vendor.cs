38a59d3380221998ecdf1acbf2c25d10dfa102f2
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-09-23 19:44:27 -0700

    loine code correction

--- SuziesCafeOrderTracker/Models/Vendor.cs
+++ SuziesCafeOrderTracker/Models/Vendor.cs
@@ -1,32 +1,12 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace VendorOrderTracker.Models
 {
-  public class Item
+  public class Vendor
   {
+    public int Id { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
-
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId - 1];
-    }
+    public List<Order> Orders { get; set; } = new List<Order>();
   }
 }
