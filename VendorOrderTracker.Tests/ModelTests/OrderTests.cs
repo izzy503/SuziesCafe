a181c6033b2c9d8d9c4126cd9a34b24ee0a48345
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-10-05 20:40:58 -0700

    added more test

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
+using System;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Tests.ModelTests
 {
@@ -16,6 +18,66 @@ namespace VendorOrderTracker.Tests.ModelTests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
+    [TestMethod]
+    public void Constructor_SetsProperties()
+    {
+      // Arrange
+      string title = "Test Order";
+      string description = "Test Description";
+      decimal price = 10.99m;
+      DateTime date = DateTime.Now;
 
+      // Act
+      Order testOrder = new Order(title, description, price, date);
+
+      // Assert
+      Assert.AreEqual(title, testOrder.Title);
+      Assert.AreEqual(description, testOrder.Description);
+      Assert.AreEqual(price, testOrder.Price);
+      Assert.AreEqual(date, testOrder.Date);
+    }
+
+    [TestMethod]
+    public void ClearAll_ClearsOrders()
+    {
+      // Arrange
+      Order.ClearAll();
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(new List<Order>(), result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllOrders()
+    {
+      // Arrange
+      Order.ClearAll();
+      Order order1 = new Order("Order 1", "Description 1", 10.99m, DateTime.Now);
+      Order order2 = new Order("Order 2", "Description 2", 19.99m, DateTime.Now);
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(new List<Order> { order1, order2 }, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsOrderById()
+    {
+      // Arrange
+      Order.ClearAll();
+      Order order1 = new Order("Order 1", "Description 1", 10.99m, DateTime.Now);
+      Order order2 = new Order("Order 2", "Description 2", 19.99m, DateTime.Now);
+
+      // Act
+      Order result = Order.Find(order2.Id);
+
+      // Assert
+      Assert.AreEqual(order2, result);
+    }
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -2,19 +2,44 @@ using System.Collections.Generic;
 
 namespace VendorOrderTracker.Models
 {
-  public class Vendor
-  {
-    public static List<Vendor> _instances = new List<Vendor> { };
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public List<Order> Orders { get; set; } = new List<Order>();
+    public class Vendor
+    {
+        public static List<Vendor> _instances = new List<Vendor>();
 
-    // create constructor
-    // create ClearAll() method
-    // create GetAll() method
-    // create Find() method
-    // create AddOrder() method
-    // Be sure to create tests for each of the above
-  }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
+
+        // Constructor
+        public Vendor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        // Method to clear all vendors
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        // Method to get all vendors
+        public static List<Vendor> GetAll()
+        {
+            return _instances;
+        }
+
+        // Method to find a vendor by Id
+        public static Vendor Find(int id)
+        {
+            return _instances.Find(vendor => vendor.Id == id);
+        }
+
+        // Method to add an order to a vendor's list of orders
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+    }
 }
