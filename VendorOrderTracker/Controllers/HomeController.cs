04cb6df98a47f3894ae4ca879147052c5ff65c34
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-10-17 18:06:56 -0700

    added more coding

--- VendorOrderTracker/Controllers/HomeController.cs
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Controllers
 {
   public class HomeController : Controller
   {
     [HttpGet("/")]
-    public IActionResult Index()
+    public ActionResult Index()
     {
       // Display the splash page
       return View();
